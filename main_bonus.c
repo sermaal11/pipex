89a2a2ea36a616a7d735610de68c7e007d041d5a
Sergio Marin Alvarez <smarin-a@c2r9s4.42madrid.com>
2024-03-06 10:40:51 +0100

    norminete pasarda

--- main_bonus.c
+++ main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: smarin-a <smarin-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 11:47:49 by sergio            #+#    #+#             */
-/*   Updated: 2024/03/05 11:58:46 by smarin-a         ###   ########.fr       */
+/*   Updated: 2024/03/06 10:40:40 by smarin-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,26 +38,27 @@ void static	ft_multipe_pipes(t_data *data, int argc, char **argv, char **env )
 	data->status = ft_close_n_wait(data);
 	ft_free(data);
 }
+
 void static	ft_here_doc(t_data *data, int argc, char **argv, char **env)
 {
 	char	*line;
 	int		tmp_fd;
-	
+
 	if (argc < 6)
 		ft_error("Error: arguments", 1);
-	tmp_fd = open(".here_doc", O_WRONLY | O_CREAT ,0644);
-	while(1)
+	tmp_fd = open(".here_doc", O_WRONLY | O_CREAT, 0644);
+	while (1)
 	{
 		line = ft_gnl(0);
 		if ((ft_strlen(line) - 1) == ft_strlen(argv[2])
 			&& ft_strncmp(line, argv[2], ft_strlen(argv[2])) == 0)
 		{
 			free(line);
-			break;
+			break ;
 		}
 		if (write(tmp_fd, line, ft_strlen(line)) == -1)
 			ft_error("Write error", 1);
-		if(line)
+		if (line)
 			free(line);
 	}
 	data->here_doc = 1;
@@ -67,6 +68,7 @@ void static	ft_here_doc(t_data *data, int argc, char **argv, char **env)
 int	main(int argc, char **argv, char **env)
 {
 	t_data	data;
+
 	if (argc < 5)
 		ft_error("Error: arguments", 1);
 	ft_memset(&data, 0, sizeof(data));
